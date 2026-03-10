e4ab8cca08c7b4f3994aaa0e2b5361fa7be477f9
Dima Kropachev <dmkropa4ev@yandex.ru>
2026-03-10 23:57:52 +0300

    update migrations

--- migrations/20260302180716_create_subscriptions.up.sql
+++ migrations/20260302180716_create_subscriptions.up.sql
@@ -2,8 +2,11 @@
 CREATE TABLE IF NOT EXISTS subscriptions (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   service TEXT NOT NULL,
-  price INTEGER NOT NULL,
+  price BIGINT NOT NULL,
   user_id UUID NOT NULL,
   start_date DATE NOT NULL,
   end_date DATE
 );
+
+-- create index
+CREATE INDEX IF NOT EXISTS ind_subscriptions_user_id ON subscriptions(user_id);
